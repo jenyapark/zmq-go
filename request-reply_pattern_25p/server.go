5b0092a2f5c6d4a0698fdd186b6c7b975d528e8c
jenyapark <awoob@khu.ac.kr>
2025-11-14 23:58:20 +0900

    25p server.go ~

--- request-reply_pattern_25p/server.go
+++ request-reply_pattern_25p/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"fmt" // print()
-	"time" // 시간 관련 함수(sleep()) 
+	"fmt"  // print()
+	"time" // 시간 관련 함수(sleep())
+
 	"github.com/pebbe/zmq4" // zmq
 )
 
 func main() {
 
-	// 소켓 생성
-	socket, err := zmq4.NewSocket(zmq4.REP)
+	// 컨텍스트 생성
+	ctx, err := zmq4.NewContext()
 	if err != nil { // err == nil -> 에러 없음
+		panic(err)
+	}
+
+	// REP 소켓 생성
+	socket, err := ctx.NewSocket(zmq4.REP)
+	if err != nil {
 		panic(err)
 	}
 
@@ -21,11 +28,20 @@ func main() {
 	}
 
 	for {
-		// wait for next request from client
+		// 메시지 수신
 		msg, err := socket.Recv(0) // 인자 0은 기본 blocking: 메시지가 올 때까지 멈춤
 		if err != nil {
 			panic(err)
 		}
 		fmt.Printf("Received request: %s\n", msg)
+
+		// 1초 대기
+		time.Sleep(1 * time.Second)
+
+		// 응답 전송
+		_, err = socket.Send("World", 0)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
